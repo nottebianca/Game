fcafa8c4d80f8b7c35b7b601d8ca01900d101706
nottebianca59 <emmkakuz@mail.ru>
2023-12-05 00:44:54 +0300

    first

--- major.py
+++ major.py
@@ -4,17 +4,43 @@ pygame.init()
 
 screen_width = 1000
 screen_height = 1000
-
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Platformer')
-
 tile_size = 50
 bg_img = pygame.image.load('img/bg.png')
 
-def draw_helper():
-	for line in range(0, 20):
-		pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
-		pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
+class Player():
+	def __init__(self, x, y):
+		img = pygame.image.load('img/santa_stand.png')
+		self.image = pygame.transform.scale(img, (40, 80))
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+		self.vel_y = 0
+		self.jumped = False
+	def update(self):
+		dx = 0
+		dy = 0
+		key = pygame.key.get_pressed()
+		if key[pygame.K_SPACE] and self.jumped == False:
+			self.vel_y = -15
+			self.jumped = True
+		if key[pygame.K_SPACE] == False:
+			self.jumped = False
+		if key[pygame.K_LEFT]:
+			dx -= 5
+		if key[pygame.K_RIGHT]:
+			dx += 5
+		self.vel_y += 1
+		if self.vel_y > 10:
+			self.vel_y = 10
+		dy += self.vel_y
+		self.rect.x += dx
+		self.rect.y += dy
+		if self.rect.bottom > screen_height:
+			self.rect.bottom = screen_height
+			dy = 0
+		screen.blit(self.image, self.rect)
 
 
 class World():
@@ -22,7 +48,6 @@ class World():
 		self.tile_list = []
 		land_img = pygame.image.load('img/land.png')
 		snowland_img = pygame.image.load('img/snowland.png')
-
 		row_count = 0
 		for row in data:
 			col_count = 0
@@ -43,7 +68,6 @@ class World():
 					self.tile_list.append(tile)
 				col_count += 1
 			row_count += 1
-
 	def draw(self):
 		for tile in self.tile_list:
 			screen.blit(tile[0], tile[1])
@@ -71,13 +95,14 @@ world_data = [
 [1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
-world = World(world_data)
+player = Player(100, screen_height - 130)
+world: World = World(world_data)
 
 run = True
 while run:
 	screen.blit(bg_img, (0, 0))
 	world.draw()
-	draw_helper()
+	player.update()
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			run = False
