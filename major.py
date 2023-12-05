b8f050aafc12232ac013aae9b4551ab104d99781
nottebianca59 <emmkakuz@mail.ru>
2023-12-05 20:04:15 +0300

    Добавление слаймов

--- major.py
+++ major.py
@@ -3,18 +3,14 @@ import random
 
 pygame.init()
 
-CLOCK = pygame.time.Clock()
-FPS = 60
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 1000
-TILE_SIZE = 50
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+fps = 60
+screen_width = 1000
+screen_height = 1000
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Platformer')
-
-BG_IMG = pygame.image.load('img/bg.png')
-LAND_IMG = pygame.image.load('img/land.png')
-SNOWLAND_IMG = pygame.image.load('img/snowland.png')
+tile_size = 50
+bg_img = pygame.image.load('img/bg.png')
 
 
 class Player:
@@ -23,14 +19,12 @@ class Player:
         self.images_left = []
         self.index = 0
         self.counter = 0
-
-        for num in range(1, 14):
+        for num in range(1, 5):
             img_right = pygame.image.load(f'img/Walk{num}.png')
-            img_right = pygame.transform.scale(img_right, (60, 60))
+            img_right = pygame.transform.scale(img_right, (40, 80))
             img_left = pygame.transform.flip(img_right, True, False)
             self.images_right.append(img_right)
             self.images_left.append(img_left)
-
         self.image = self.images_right[self.index]
         self.rect = self.image.get_rect()
         self.rect.x = x
@@ -46,32 +40,26 @@ class Player:
         dy = 0
         walk_cooldown = 5
         key = pygame.key.get_pressed()
-
         if key[pygame.K_SPACE] and not self.jumped:
             self.vel_y = -15
             self.jumped = True
-
         if not key[pygame.K_SPACE]:
             self.jumped = False
-
         if key[pygame.K_LEFT]:
             dx -= 5
             self.counter += 1
             self.direction = -1
-
         if key[pygame.K_RIGHT]:
             dx += 5
             self.counter += 1
             self.direction = 1
-
         if not key[pygame.K_LEFT] and not key[pygame.K_RIGHT]:
             self.counter = 0
             self.index = 0
             if self.direction == 1:
                 self.image = self.images_right[self.index]
-            elif self.direction == -1:
+            if self.direction == -1:
                 self.image = self.images_left[self.index]
-
         if self.counter > walk_cooldown:
             self.counter = 0
             self.index += 1
@@ -79,15 +67,12 @@ class Player:
                 self.index = 0
             if self.direction == 1:
                 self.image = self.images_right[self.index]
-            elif self.direction == -1:
+            if self.direction == -1:
                 self.image = self.images_left[self.index]
-
         self.vel_y += 1
         if self.vel_y > 10:
             self.vel_y = 10
-
         dy += self.vel_y
-
         for tile in world.tile_list:
             if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
                 dx = 0
@@ -98,37 +83,40 @@ class Player:
                 elif self.vel_y >= 0:
                     dy = tile[1].top - self.rect.bottom
                     self.vel_y = 0
-
         self.rect.x += dx
         self.rect.y += dy
-
-        if self.rect.bottom > SCREEN_HEIGHT:
-            self.rect.bottom = SCREEN_HEIGHT
+        if self.rect.bottom > screen_height:
+            self.rect.bottom = screen_height
             dy = 0
-
         screen.blit(self.image, self.rect)
 
 
 class World:
     def __init__(self, data):
         self.tile_list = []
-
+        land_img = pygame.image.load('img/land.png')
+        snowland_img = pygame.image.load('img/snowland.png')
         row_count = 0
         for row in data:
             col_count = 0
             for tile in row:
                 if tile == 1:
-                    img = pygame.transform.scale(LAND_IMG, (TILE_SIZE, TILE_SIZE))
+                    img = pygame.transform.scale(land_img, (tile_size, tile_size))
                     img_rect = img.get_rect()
-                    img_rect.x = col_count * TILE_SIZE
-                    img_rect.y = row_count * TILE_SIZE
-                    self.tile_list.append((img, img_rect))
-                elif tile == 2:
-                    img = pygame.transform.scale(SNOWLAND_IMG, (TILE_SIZE, TILE_SIZE))
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 2:
+                    img = pygame.transform.scale(snowland_img, (tile_size, tile_size))
                     img_rect = img.get_rect()
-                    img_rect.x = col_count * TILE_SIZE
-                    img_rect.y = row_count * TILE_SIZE
-                    self.tile_list.append((img, img_rect))
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 3:
+                    blob = Enemy(col_count * tile_size, row_count * tile_size + 15)
+                    blob_group.add(blob)
                 col_count += 1
             row_count += 1
 
@@ -146,16 +134,36 @@ class Snowflake:
 
     def update(self):
         self.y += self.speed
-        if self.y > SCREEN_HEIGHT:
+        if self.y > screen_height:
             self.y = -50
-            self.x = random.randint(0, SCREEN_WIDTH)
+            self.x = random.randint(0, screen_width)
 
     def draw(self):
         pygame.draw.circle(screen, (255, 255, 255), (self.x, self.y), self.size)
 
 
-snowflakes = [Snowflake(random.randint(0, SCREEN_WIDTH), random.randint(0, SCREEN_HEIGHT),
+snowflakes = [Snowflake(random.randint(0, screen_width), random.randint(0, screen_height),
                         random.randint(2, 5), random.randint(1, 4)) for _ in range(100)]
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        img = pygame.image.load('img/slime_mob.png')
+        self.image = pygame.transform.scale(img, (40, 40))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.move_direction = 1
+        self.move_counter = 0
+
+    def update(self):
+        self.rect.x += self.move_direction
+        self.move_counter += 1
+        if abs(self.move_counter) > 50:
+            self.move_direction *= -1
+            self.move_counter *= -1
+
 
 world_data = [
 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -180,27 +188,25 @@ world_data = [
 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
-player = Player(100, SCREEN_HEIGHT - 130)
+player = Player(100, screen_height - 130)
+blob_group = pygame.sprite.Group()
 world = World(world_data)
 
 run = True
 while run:
-    CLOCK.tick(FPS)
-
+    clock.tick(fps)
     for snowflake in snowflakes:
         snowflake.update()
-
-    screen.blit(BG_IMG, (0, 0))
+    screen.blit(bg_img, (0, 0))
     world.draw()
+    blob_group.update()
+    blob_group.draw(screen)
     player.update()
-
     for snowflake in snowflakes:
         snowflake.draw()
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
     pygame.display.update()
 
 pygame.quit()
